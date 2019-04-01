55b35016d3279375525b803434bb16676cd14aa6
Jack Slater <jslater@thoughtworks.com>
2019-04-01 10:48:24 +0100

    Add winston logger

--- app/src/router/app.ts
+++ app/src/router/app.ts
@@ -1,25 +1,30 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {knex} from "../../../db/knex"
-import morgan from "morgan"
-import {asyncMiddleware, clientErrHandler, serverErrHander} from "./middleware"
+import {asyncMiddleware, clientErrHandler, serverErrHander, winstonErrorLogger, winstonReqLogger} from "./middleware"
 import {registerUser} from "../service/register"
 import {Db} from "../postgres/Db"
 import {User} from "../types/"
 import {userValidationHandler} from "./validation"
+import morgan from "morgan"
 
 const app: express.Application = express();
+const router: express.Router = express.Router();
 const db: Db = new Db(knex)
 
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(winstonReqLogger)
 
-app.post('/register', userValidationHandler, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+router.post('/register', userValidationHandler, asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
     const payload: User = req.body
     const jwt = await registerUser(payload, db)
     res.status(201).send(jwt)
 
 }))
 
+app.use(router)
+
+app.use(winstonErrorLogger)
 app.use(clientErrHandler)
 app.use(serverErrHander)
 
--- app/src/router/middleware.ts
+++ app/src/router/middleware.ts
@@ -1,10 +1,11 @@
 import {NextFunction, Request, Response} from "express"
 import {ErrorType, FormattedError} from "../types"
 import {validationResult} from "express-validator/check"
+import expressWinston from "express-winston"
+import * as winston from "winston"
 
 export const clientErrHandler = (err: FormattedError, req: Request, res: Response, next: NextFunction) => {
     if (err.statusCode) {
-        console.error("%s: %s: %s: %s", err.statusCode, err.cause, err.message, err.stack)
         res.status(err.statusCode).send(err)
     } else {
         next(err)
@@ -13,7 +14,6 @@ export const clientErrHandler = (err: FormattedError, req: Request, res: Respons
 
 export const serverErrHander = (err: Error, req: Request, res: Response, next: NextFunction) => {
     const errorMsg = new FormattedError(ErrorType.SERVER_ERROR, err.message)
-    console.error("%s: %s: %s: %s", errorMsg.statusCode, errorMsg.cause, err.message, err.stack)
     res.status(errorMsg.statusCode).send(errorMsg)
 }
 
@@ -24,3 +24,30 @@ export const asyncMiddleware = (fn: (req: Request, res: Response, next: NextFunc
             "Validation Error invalid payload", errors.array()))
         else Promise.resolve(fn(req, res, next)).catch(next);
 };
+
+export const winstonErrorLogger = expressWinston.errorLogger({
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            filename: './logs/error.log'
+        })
+    ],
+    // @ts-ignore
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    )
+})
+
+export const winstonReqLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.File({
+            filename: './logs/combined.log'
+        })
+    ],
+    // @ts-ignore
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    )
+})
