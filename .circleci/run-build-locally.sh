77a81ead9e9f5700e4c82d2906eb83989d615f09
Jack Slater <jslater@thoughtworks.com>
2019-04-02 10:16:22 +0100

    Update npm test command

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=1e4d511f96608be5054e822014a86a4121be0044\
-    --form config=./config.yml \
+    --form revision=96d252fcd14689d2508a8076928fc282fb83a13f\
+    --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/jack-slater/bookclub-auth/tree/master
